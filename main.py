b7db1c43106ca15f2c8c77d62454c1a0e5b217b5
Eniakz <eniakz@icloud.com>
2023-01-22 19:55:48 +0100

    fix bugs with cards counting and add computer's hit

--- main.py
+++ main.py
@@ -15,33 +15,55 @@ def card_value(card):
     else:
         return card
 
-player = []
-comp = []
+def play():
+    player = []
+    comp = []
 
-player.append(draw_a_card())                                        # draw a cards for player
-player.append(draw_a_card())
+    player.append(draw_a_card())                                        # draw a cards for player
+    player.append(draw_a_card())
 
-player_score = card_value(player[0]) + card_value(player[1])        # total cards value
-print(f"Your cards: [{player[0]}], [{player[1]}] | current score: {player_score}")
+    player_score = card_value(player[0]) + card_value(player[1])        # total cards value
+    print(f"Your cards: {player} | current score: {player_score}")
 
+    comp.append(draw_a_card())                                          # draw a cards for computer
+    comp.append(draw_a_card())
 
-comp.append(draw_a_card())                                          # draw a cards for computer
-comp.append(draw_a_card())
+    comp_score = card_value(comp[0]) + card_value(comp[1])              # total cards value
+    print(f"Computer's first card: {comp[0]} | value: {card_value(comp[0])}")
 
-comp_score = card_value(comp[0]) + card_value(comp[1])              # total cards value
-print(f"Computer's first card: {comp[0]} | value: {card_value(comp[0])}")
+    while(player_score < 21):
+        hit = input("Type 'y' to get another card or type 'n' to pass: ").lower()       # add another card
+        if hit == "y":
+            player.append(draw_a_card())
+            player_score += card_value(player[-1])
+            if player_score > 21:
+                # TODO: check if ace in hand and convert to 1-value
+                print(f"Your final hand: {player} | final score: {player_score}")   # player went over
+                print(f"Computer's final hand: {comp} | final score: {comp_score}")
+                print("You went over and you lose.")
+                # TODO: go to play_again function
 
-while(player_score < 21):
-    hit = input("Type 'y' to get another card or type 'n' to pass: ").lower()       # add another card
-    if hit == "y":
-        player.append(draw_a_card())
-        player_score += card_value(player[2])
-        if player_score > 21:
-            print(f"Your final hand: {player} | final score: {player_score} \nYou went over and you lose.")     # player went over
-            # TODO: Show computer's 2nd card and go to play_again function
-        elif player_score == 21:
-            # TODO: Check computer
-            print(f"Your cards: {player} | final score: {player_score}")
+            elif player_score == 21:
+                print(f"Your cards: {player} | final score: {player_score}")
+                while (comp_score < 21):                                            # computer's hit
+                    # TODO: Check computer
+                    comp.append(draw_a_card())
+                    comp_score += card_value(comp[-1])
+                    if comp_score > 21:
+                        # TODO: check if ace in comp's hand and convert to 1-value
+                        print(f"Your final hand: {player} | final score: {player_score}")
+                        print(f"Computer's final hand: {comp} | final score: {comp_score}")     # computer went over
+                        print("Opponent went over, you win!")
+
+                if comp_score == 21:
+                    # TODO: print draw and go to play_again function
+                    pass
+
+            else:                                                               # player's score < 21 => ask for hit
+                print(f"Your cards: {player} | current score: {player_score}")
         else:
             # TODO: Stand
             print(f"Your cards: {player} | current score: {player_score}")
+
+
+play()
